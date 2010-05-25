94e9d2a5009fea780b2b6fc35d64228215d6f8a4
NOMURA Yoshihide <nomura@pobox.com>
2010-05-25 11:46:36 -0700

    change to generate menu.lst from config files.

--- mkboot.sh
+++ mkboot.sh
@@ -5,6 +5,8 @@ cd `dirname $0`
 
 WORKROOT=/tmp
 USBROOT=$(pwd)
+MENUFILE=menu.lst
+BOOTFILE=boot.lst
 
 prepareiso()
 {
@@ -51,10 +53,11 @@ geninitrd()
 
 geninitrd_centos()
 {
-    local ISO=$2
-    local DST=centos
-    local PATCH=$3
-    local VER=$1
+    # for CentOS and Fedora
+    local ISO=$3
+    local DST=$1
+    local PATCH=$4
+    local VER=$2
 
     mkdir -pv /mnt/iso $DST || exit 1
     mount -v -o loop -t iso9660 iso/$ISO /mnt/iso || exit 1
@@ -85,24 +88,6 @@ geninitrd_knoppix()
     rm $DST/minirt.gz
 }
 
-geninitrd_fedora()
-{
-    local ISO=$2
-    local DST=fedora
-    local PATCH=$3
-    local VER=$1
-
-    mkdir -pv /mnt/iso $DST || exit 1
-    mount -v -o loop -t iso9660 iso/$ISO /mnt/iso || exit 1
-    cp -v /mnt/iso/isolinux/vmlinuz0 $DST/vmlinuz-$VER
-    cp -v /mnt/iso/isolinux/initrd0.img $DST
-    umount -v /mnt/iso
-    rmdir /mnt/iso
-    # apply patch
-    geninitrd $DST $VER initrd0.img $PATCH
-    rm $DST/initrd0.img
-}
-
 copyiso()
 {
     local ISO=$2
@@ -119,21 +104,38 @@ copyiso()
     rmdir /mnt/iso
 }
 
-copyknoppix()
+addboot()
 {
-    local ISO=$2
-    local DST=$1
-
-    if [ -d $DST ]; then
-	# clean previous folder first
-	rm -rv $DST
+    local BOOT=$1
+    if [ ! -f cfg/${BOOT}.cfg ]; then
+	echo "Configuration file is not found for ${BOOT}."
+	exit 1
     fi
-    mkdir -pv /mnt/iso $DST || exit 1
-    mount -v -o loop iso/$ISO /mnt/iso || exit 1
-    cp -rv /mnt/iso/KNOPPIX/* $DST
-    cp -rv /mnt/iso/boot $DST
-    umount -v /mnt/iso
-    rmdir /mnt/iso
+    if [ ! -f $BOOTFILE ]; then
+	touch $BOOTFILE || exit 1
+    fi
+    if [ -z $(grep "$BOOT" $BOOTFILE) ]; then
+	echo "$BOOT" >> $BOOTFILE
+	echo "$BOOT was added to boot list."
+	genmenu
+    else
+	echo "$BOOT is already installed."
+    fi
+}
+
+genmenu()
+{
+    # sort the order
+    sort $BOOTFILE > ${BOOTFILE}.new
+    mv -fv ${BOOTFILE}.new ${BOOTFILE}
+    # generate menu file
+    cat cfg/head.cfg > $MENUFILE
+    while read line; do
+	cat cfg/${line}.cfg >> $MENUFILE
+	echo "" >> $MENUFILE
+    done < $BOOTFILE
+    cat cfg/tail.cfg >> $MENUFILE
+    echo "New menu was generated."
 }
 
 if [ -z "$1" ]; then
@@ -149,52 +151,64 @@ fi
 DISTRO=$1
 
 case $DISTRO in
+    genmenu)
+	genmenu
+	;;
     ubuntu-8.04)
 	ISOFILE=ubuntu-8.04.4-desktop-i386.iso
 	BASEURL=http://releases.ubuntu.com/8.04.4
 	prepareiso $ISOFILE $BASEURL
+	addboot ubuntu-8.04
 	;;
     ubuntu-9.04)
 	ISOFILE=ubuntu-9.04-desktop-i386.iso
 	BASEURL=http://releases.ubuntu.com/9.04
 	prepareiso $ISOFILE $BASEURL
+	addboot ubuntu-9.04
 	;;
     ubuntu-9.10)
 	ISOFILE=ubuntu-9.10-desktop-i386.iso
 	BASEURL=http://releases.ubuntu.com/9.10
 	prepareiso $ISOFILE $BASEURL
+	addboot ubuntu-9.10
 	;;
     ubuntu-10.04)
 	ISOFILE=ubuntu-10.04-desktop-i386.iso
 	BASEURL=http://releases.ubuntu.com/10.04
 	prepareiso $ISOFILE $BASEURL
+	addboot ubuntu-10.04
 	;;
     mint-9)
 	ISOFILE=linuxmint-9-gnome-cd-i386.iso
 	BASEURL=http://mirror.amarillolinux.com/linuxmint/stable/9
 	prepareiso $ISOFILE $BASEURL
+	addboot mint-9
 	;;
-    dsl)
+    dsl|dsl-4.4.10)
 	ISOFILE=dsl-4.4.10-initrd.iso
 	BASEURL=http://distro.ibiblio.org/pub/linux/distributions/damnsmall/current
 	prepareiso $ISOFILE $BASEURL
+	addboot dsl-4.4.10
 	;;
-    netbootcd)
+    netbootcd|netbootcd-3.2.1)
 	ISOFILE=NetbootCD-3.2.1.iso
 	BASEURL=http://downloads.tuxfamily.org/netbootcd/3.2.1
 	prepareiso $ISOFILE $BASEURL
+	addboot netbootcd-3.2.1
 	;;
     fedora-12)
 	ISOFILE=Fedora-12-i686-Live.iso
 	BASEURL=http://download.fedoraproject.org/pub/fedora/linux/releases/12/Live/i686
 	prepareiso $ISOFILE $BASEURL
-	geninitrd_fedora 12 $ISOFILE fedora/fedora-12.patch
+	geninitrd_centos fedora 12 $ISOFILE fedora/fedora-12.patch
+	addboot fedora-12
 	;;
     soas2)
 	ISOFILE=soas-2-blueberry.iso
 	BASEURL=http://download.sugarlabs.org/soas/releases
 	prepareiso $ISOFILE $BASEURL
-	copyiso $DISTRO $ISOFILE
+	copyiso soas2 $ISOFILE
+	addboot soas2
 	;;
     knoppix-jp|knoppix-6.0.1-jp)
 	# japanese version
@@ -202,29 +216,44 @@ case $DISTRO in
 	BASEURL=http://ring.aist.go.jp/pub/linux/knoppix/iso
 	prepareiso $ISOFILE $BASEURL
 	geninitrd_knoppix 6.0.1-jp $ISOFILE knoppix/knoppix-6.0.1-jp.patch
+	addboot knoppix-6.0.1-jp
+	;;
+    knoppix-6.2.1)
+	ISOFILE=KNOPPIX_V6.2.1CD-2010-01-31-EN.iso 
+	BASEURL=http://ftp.knoppix.nl/os/Linux/distr/knoppix
+	prepareiso $ISOFILE $BASEURL
+	geninitrd_knoppix 6.2.1 $ISOFILE knoppix/knoppix-6.0.1-jp.patch
+	addboot knoppix-6.2.1
 	;;
     rescue|rescue-1.5.4)
 	ISOFILE=systemrescuecd-x86-1.5.4.iso
 	BASEURL=http://downloads.sourceforge.net/project/systemrescuecd/sysresccd-x86/1.5.4
 	prepareiso $ISOFILE $BASEURL
-	copyiso $DISTRO $ISOFILE
+	copyiso rescue-1.5.4 $ISOFILE
+	addboot systemrescuecd-1.5.4
 	;;
     centos-5|centos-5.5)
 	ISOFILE=CentOS-5.5-i386-LiveCD.iso
 	BASEURL=http://ftp.osuosl.org/pub/centos/5.5/isos/i386
 	prepareiso $ISOFILE $BASEURL
-	geninitrd_centos 5.5 $ISOFILE centos/centos-5.5.patch
+	geninitrd_centos centos 5.5 $ISOFILE centos/centos-5.5.patch
+	addboot centos-5.5
 	;;
-    opensuse-11)
+    opensuse-11|opensuse-11.2)
 	ISOFILE=openSUSE-11.2-GNOME-LiveCD-i686.iso
 	BASEURL=http://download.opensuse.org/distribution/11.2/iso
 	prepareiso $ISOFILE $BASEURL
 	copyiso opensuse-11.2 $ISOFILE
+	addboot opensuse-11.2
 	;;
-    puppy-5)
+    puppy-5|puppy-5.00)
 	ISOFILE=lupu-500.iso
 	BASEURL=http://distro.ibiblio.org/pub/linux/distributions/puppylinux/puppy-5.0
 	prepareiso $ISOFILE $BASEURL
-	copyiso $DISTRO $ISOFILE
+	copyiso puppy-5.00 $ISOFILE
+	addboot puppy-5.00
+	;;
+    *)
+	echo "$DISTRO is not supported."
 	;;
 esac
